cf16bc88c6b937d34a6ed57d3d98c1a23059b8f1
Roland Bernard <rolli.bernard@gmail.com>
2023-06-08 13:07:00 +0200

    Starting with the evaluation

--- experiments/scripts/gen-repairs.sh
+++ experiments/scripts/gen-repairs.sh
@@ -2,47 +2,92 @@
 # Given the ontologies as input, make each of them inconsistent and the repair the again using
 # maximal consistent sets and iterated weakening. Save the results of all repairs in subfolders.
 
-REPAIRS_PER_RUN=50
+REPAIRS_PER_RUN=100
+REASONER=fact++
 
 function repair-mcs() {
-    systemd-run --scope -p MemoryMax=10G --user \
-        java -cp target/shaded-ontologyutils-0.0.1.jar www.ontologyutils.apps.RepairMcs \
-        --compute=sample --limit $REPAIRS_PER_RUN -o $2 $1 >$3 2>&1
+    if timeout 10m \
+        systemd-run --scope -p MemoryMax=10G --user \
+            java -cp target/shaded-ontologyutils-0.0.1.jar -Xms9G www.ontologyutils.apps.RepairMcs \
+            --compute=sample -v --reasoner=$REASONER --limit $REPAIRS_PER_RUN -o $2 $1 >$3 2>&1
+    then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function repair-weakening() {
-    systemd-run --scope -p MemoryMax=10G --user \
-        java -cp target/shaded-ontologyutils-0.0.1.jar www.ontologyutils.apps.RepairWeakening \
-        --preset=bernard2023 --limit $REPAIRS_PER_RUN -o $2 $1 >$3 2>&1
+    if timeout 10m \
+        systemd-run --scope -p MemoryMax=10G --user \
+            java -cp target/shaded-ontologyutils-0.0.1.jar -Xms9G www.ontologyutils.apps.RepairWeakening \
+            --preset=bernard2023 -v --reasoner=$REASONER --limit $REPAIRS_PER_RUN -o $2 $1 >$3 2>&1
+    then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function make-inconsistent() {
-    systemd-run --scope -p MemoryMax=10G --user \
-        java -cp target/shaded-ontologyutils-0.0.1.jar www.ontologyutils.apps.MakeInconsistent \
-        --strict-sroiq --strict-simple-roles --simple-ria-weakening -o $2 $1 >$3 2>&1
+    if timeout 5m \
+        systemd-run --scope -p MemoryMax=10G --user \
+        java -cp target/shaded-ontologyutils-0.0.1.jar -Xms9G www.ontologyutils.apps.MakeInconsistent \
+        --strict-sroiq --strict-simple-roles --simple-ria-weakening --strict-owl2 -v --reasoner=$REASONER -o $2 $1 >$3 2>&1
+    then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function classify-ontology() {
     systemd-run --scope -p MemoryMax=10G --user \
-        java -cp target/shaded-ontologyutils-0.0.1.jar www.ontologyutils.apps.ClassifyOntology \
-        $1 >$2 2>&1
+        java -cp target/shaded-ontologyutils-0.0.1.jar -Xms9G www.ontologyutils.apps.ClassifyOntology \
+        $1 >>$2 2>&1
 }
 
 function run-experiment() {
     run=$(date --iso-8601=seconds)
-    onto_name=$(basename $1 .owl)
-    out_dir=experiments/repairs/$onto_name/$run/
+    onto=$1
+    onto_name=$(basename $onto .owl)
+    out_dir=experiments/repairs/$onto_name/$run
+    success=0
 
     mkdir -p $out_dir/{mcs,weakening}
-    make-inconsistent $1 $out_dir/inconsistent.owl $out_dir/make-inconsistent.log
-    classify-ontology $out_dir/inconsistent.owl $out_dir/ontology-info.txt
-
-    repair-mcs $out_dir/inconsistent.owl $out_dir/mcs/repair.owl $out_dir/repair-mcs.log
-    repair-weakening $out_dir/inconsistent.owl $out_dir/weakening/repair.owl $out_dir/repair-weakening.log
+    mkdir -p experiments/repairs/.failed/$onto_name
+    if make-inconsistent $onto $out_dir/inconsistent.owl $out_dir/make-inconsistent.log
+    then
+        classify-ontology $out_dir/inconsistent.owl $out_dir/ontology-info.txt
+        if repair-mcs $out_dir/inconsistent.owl $out_dir/mcs/repair.owl $out_dir/repair-mcs.log
+        then
+            if repair-weakening $out_dir/inconsistent.owl $out_dir/weakening/repair.owl $out_dir/repair-weakening.log
+            then
+                success=1
+            fi
+        fi
+    fi
+    if [ $success == 0 ]
+    then
+        mv $out_dir experiments/repairs/.failed/$onto_name/$run
+        return 1
+    else
+        return 0
+    fi
 }
 
-for onto in $@
+todo=$@
+
+while [ "$todo" ]
 do
-    run-experiment $onto
+    redo=$todo
+    todo=
+    for onto in $redo
+    do
+        if ! run-experiment $onto
+        then
+            todo="$todo $onto"
+        fi
+    done
 done
 
